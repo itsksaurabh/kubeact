5145d67d259a25b79d76ef7671fdaaf45ed6a73c
itsksaurabh <itsksaurabh@gmail.com>
2020-03-30 14:44:46 +0530

    add sample test files

---
+++ main.go
@@ -0,0 +1,13 @@
+// This file is only for testing purposes
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("kubeact")
+}
+
+// Add returns sum of two int
+func Add(x, y int) int {
+	return x + y
+}
