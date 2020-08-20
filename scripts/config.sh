29d796a8bffaebfbd3e19b1d35d239a8c95851df
Kumar Saurabh <itsksaurabh@gmail.com>
2020-08-20 12:55:22 +0530

    add support for latest runner v2.263.0

--- scripts/config.sh
+++ scripts/config.sh
@@ -11,8 +11,8 @@ REGISTRATION_TOKEN=$(echo $temp | grep "token" | awk '{print $3}'| awk -F , '{pr
 # Create a folder
 mkdir actions-runner && cd actions-runner
 # Download the latest runner package
-curl -O -L https://github.com/actions/runner/releases/download/v2.165.2/actions-runner-linux-x64-2.165.2.tar.gz
+curl -O -L https://github.com/actions/runner/releases/download/v2.263.0/actions-runner-linux-x64-2.263.0.tar.gz
 # Extract the installer
-tar xzf ./actions-runner-linux-x64-2.165.2.tar.gz
+tar xzf ./actions-runner-linux-x64-2.263.0.tar.gz
 # Configure the runner
 ./config.sh --url https://github.com/${GITHUB_USERNAME}/${GITHUB_REPO_NAME} --token $REGISTRATION_TOKEN <<< $'\n\n' 
